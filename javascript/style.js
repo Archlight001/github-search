64db72dac959028f904582dcf6f544b243b7216d
Light <archlight001@gmail.com>
2021-05-25 10:39:25 +0100

    Add status icon details to profile details

--- javascript/style.js
+++ javascript/style.js
@@ -1,10 +1,20 @@
 var inputSearch =  getComputedStyle(document.getElementsByClassName("input__search")[0]);
-console.log(inputSearch.width)
 
 var input__submit = document.getElementsByClassName("input__submit");
 function changeColor(){
     if(inputSearch.width>"100px"){
         input__submit[0].style.display = "none"
 
+    }
+}
+
+var statusWidth = getComputedStyle(document.getElementsByClassName("status")[0]).width
+var statusInfo = document.getElementsByClassName("status__info")[0];
+
+function showSpan(status){
+    if(status === true){
+        statusInfo.style.display = "block"
+    }else{
+        statusInfo.style.display = "none"
     }
 }
